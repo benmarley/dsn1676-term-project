3c5f49c998a5d6c3977c5515495f6920cc3ccb4a
benmarley <benmarley@Bens-MacBook-Pro-3.local>
2015-03-13 15:30:14 -0400

    fixed navigation added X button

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ var $navBtn = $('.nav-button')
 var $nav = $('.navigation')
 
 $navBtn.on('click', function () {
-	$nav.toggleClass('navigation-open');
+	$nav.addClass('navigation-open');
 });
+
+$navBtn.on('click', function (){
+	$navBtn.addClass('nav-none')
+});
+
