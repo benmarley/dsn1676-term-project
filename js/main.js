bd7f43952dae0d995a932a6b381b667aa1db116b
benmarley <benmarley@Bens-MacBook-Pro-3.local>
2015-03-18 15:56:23 -0400

    anchor drop works/ not check of all sizes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ var $nav = $('.navigation');
 
 var $xButton = $('.x-button');
 
+var $anchor = $('.anchor-graphic');
+var $anchorButton = $('.anchor-button');
+
 $navBtn.on('click', function () {
 	$nav.addClass('navigation-open');
 });
@@ -17,6 +20,11 @@ $xButton.on('click', function () {
 
 $xButton.on('click', function () {
 	$navBtn.toggleClass('nav-none');
+});
+
+$anchorButton.on('click', function () {
+	$anchor.toggleClass('anchor-drop')
+
 });
 
 
