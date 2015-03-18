765a4d2d6c52a123e51574953221405bfaac06f1
benmarley <benmarley@Bens-MacBook-Pro-3.local>
2015-03-17 23:31:57 -0400

    added function "X" button, also added functional transition for nav

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-var $navBtn = $('.nav-button')
-var $nav = $('.navigation')
+var $navBtn = $('.nav-button');
+var $nav = $('.navigation');
+
+var $xButton = $('.x-button');
 
 $navBtn.on('click', function () {
 	$nav.addClass('navigation-open');
 });
 
-$navBtn.on('click', function (){
-	$navBtn.addClass('nav-none')
+$navBtn.on('click', function () {
+	$navBtn.addClass('nav-none');
 });
+
+$xButton.on('click', function () {
+	$nav.toggleClass('navigation-open');
+});
+
+$xButton.on('click', function () {
+	$navBtn.toggleClass('nav-none');
+});
+
 
